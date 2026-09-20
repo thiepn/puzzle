9c32a6a3b8839add347a951986a2dd6088c824a3
thiepn <junsosmart@gmail.com>
2026-09-21 00:21:59 +0200

    Release 1.8.0 service-worker shell

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* Puzzle Arcade 1.7.0: variety and anti-repetition release shell. */
-const APP_VERSION = '1.7.0';
-const CACHE_VERSION = 'v26';
+/* Puzzle Arcade 1.8.0: generator robustness and long-run reliability release shell. */
+const APP_VERSION = '1.8.0';
+const CACHE_VERSION = 'v27';
 const SCOPE = new URL(self.registration.scope);
 const CACHE_FAMILY = 'puzzle-arcade-core-';
 const CACHE_PREFIX = `${CACHE_FAMILY}${encodeURIComponent(SCOPE.pathname)}-`;
